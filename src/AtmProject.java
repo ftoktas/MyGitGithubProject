0ed9f42508ec35c5b53e16683256b4fd33d7a561
ftoktas <ftoktas@gmail.com>
2023-04-26 22:00:18 +0300

    v2

---
+++ src/AtmProject.java
@@ -0,0 +1,16 @@
+public class AtmProject{
+    public static void main(String[] args) {
+
+        System.out.println("Bu bir ATM projesidir");
+        // version1
+        System.out.println("Java is easy");
+        //version2
+
+
+    }
+
+
+
+
+
+}
