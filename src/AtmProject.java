8c86f69bafed25dcd4850396c5da4192198e3479
ftoktas <ftoktas@gmail.com>
2023-04-26 22:03:02 +0300

    v4

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -7,6 +7,8 @@ public class AtmProject{
         //version2
         System.out.println("Learn Java earn money");
         //version3
+        System.out.println("Git ogrenmek cok kolay");
+        //version4
 
 
     }
